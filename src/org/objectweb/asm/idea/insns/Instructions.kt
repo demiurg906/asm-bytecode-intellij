212e0d677e2f125dfb0a51382c795912ac3701fd
Zubakov Aleksey <aleks.zubakov@gmail.com>
2018-05-04 20:26:33 +0300

    added jump and compare instructions

--- src/org/objectweb/asm/idea/insns/Instructions.kt
+++ src/org/objectweb/asm/idea/insns/Instructions.kt
@@ -20,3 +20,51 @@ data class IntConst(override val opcode: Int, val operand: Int) : Insn(opcode)
 data class DoubleConst(override val opcode: Int, val operand: Double) : Insn(opcode)
 data class LongConst(override val opcode: Int, val operand: Long) : Insn(opcode)
 data class FloatConst(override val opcode: Int, val operand: Float) : Insn(opcode)
+
+
+enum class ComparatorType {
+    LESS, LESS_EQUAL, GREATER,
+    GREATER_EQUAL, EQUAL, NOT_EQUAL
+}
+
+/**
+ * ComareOperation holds commands:
+ * LCMP — push 0 if the two longs are the same, 1 if value1 is greater than value2, -1 otherwise
+ * FCMPL — compare two floats
+ * FCMPG — compare two floats
+ * DCMPL — compare two doubles
+ * DCMPG — compare two doubles
+ */
+data class CompareOperation(override val opcode: Int, val comparatorType: ComparatorType, val type: PrimitiveType) : Insn(opcode)
+
+
+// !! has reference to another command
+// if_icmpeq
+// if_icmpge
+// if_icmpgt
+// if_icmple
+// if_icmplt
+// if_icmpne
+data class IntCompareJump(override val opcode: Int, val comparatorType: ComparatorType) : Insn(opcode)
+
+// !! has reference to another command
+// ifeq
+// ifge
+// ifgt
+// ifle
+// iflt
+data class ZeroCompareJump(override val opcode: Int, val comparatorType: ComparatorType) : Insn(opcode)
+
+// !! has reference to another command
+// ifnonnull
+// ifnull
+/**
+ * [comparatorType] has value EQUAL if equals to null operation,
+ * NOT_EQUAL otherwise
+ */
+data class NullCompareJump(override val opcode: Int, val comparatorType: ComparatorType) : Insn(opcode)
+
+
+
+data class Goto(override val opcode: Int): Insn(opcode)
+// TODO if_acmpne
