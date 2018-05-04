3afb4065d5fbf21258d775c2a815a2c2b84de900
Dmitriy Novozhilov <dimon2x@gmail.com>
2018-05-04 10:43:28 +0300

    icon added to toolbar action

--- src/org/objectweb/asm/idea/actions/stackActions.kt
+++ src/org/objectweb/asm/idea/actions/stackActions.kt
@@ -1,11 +1,12 @@
 package org.objectweb.asm.idea.actions
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import org.objectweb.asm.idea.stackmachine.StackMachineService
 
-class StartNewStackAction : AnAction() {
+class StartNewStackAction : AnAction("Init stack", "", AllIcons.General.Run) {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         val service = StackMachineService.getInstance(project)
@@ -14,5 +15,7 @@ class StartNewStackAction : AnAction() {
         }
     }
 
+    override fun toString(): String = "Init stack"
 
+    override fun displayTextInToolbar(): Boolean = true
 }
