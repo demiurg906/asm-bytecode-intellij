7dc243cf80fb5d5f251ee76bfedf40a6dfa9c900
Dmitriy Novozhilov <dimon2x@gmail.com>
2018-05-04 14:34:25 +0300

    `StackMachineService` refactored

--- src/org/objectweb/asm/idea/stackmachine/StackMachineServiceImpl.kt
+++ src/org/objectweb/asm/idea/stackmachine/StackMachineServiceImpl.kt
@@ -6,13 +6,14 @@ import org.objectweb.asm.idea.ui.StackViewer
 
 class StackMachineServiceImpl : StackMachineService {
     override fun initializeClass(map: CommandsMap) {
-        _stackMachine = StackMachine.getInstance()
+        stackMachine = StackMachine.getInstance()
         commandsMap = map
     }
 
-    private var _stackMachine: StackMachine = StackMachine.getInstance()
+    private var stackMachine: StackMachine = StackMachine.getInstance()
     private var commandsMap: CommandsMap = mapOf()
-    override val stackViewer: StackViewer
+
+    private val stackViewer: StackViewer
         get() = _stackViewer!!
     private var _stackViewer: StackViewer? = null
 
@@ -20,29 +21,33 @@ class StackMachineServiceImpl : StackMachineService {
         get() = _editor!!
     private var _editor: Editor? = null
 
-    override val currentLine: Int
-        get() = editor.caretModel.logicalPosition.line
-
-    override val stackMachine: StackMachine
-        get() = _stackMachine
+    private var currentLine: Int = 0
 
     override fun emulateMachineUntil() {
-        for (line in 0 until currentLine) {
+        val caretLine = editor.caretModel.currentCaret.logicalPosition.line
+        for (line in currentLine..caretLine) {
             val command = commandsMap[line] ?: throw StackEvaluationException("no command under line $line")
-            _stackMachine.execute(command)
+            stackMachine.execute(command)
         }
+        currentLine = caretLine
     }
 
     override fun emulateOneLine() {
         val command = commandsMap[currentLine] ?: throw StackEvaluationException("no command under line $currentLine")
-        _stackMachine.execute(command)
+        stackMachine.execute(command)
         moveCaretToNextLine()
+        visualizeStack()
+    }
+
+    private fun visualizeStack() {
+        // TODO: implement
     }
 
     private fun moveCaretToNextLine() {
         val caret = editor.caretModel.currentCaret
         val currentPosition = caret.logicalPosition
-        caret.moveToLogicalPosition(LogicalPosition(currentPosition.line + 1, currentPosition.column))
+        caret.moveToLogicalPosition(LogicalPosition(currentPosition.line + 1, 0))
+        currentLine = caret.logicalPosition.line
     }
 
     override fun registerStackViewer(stackViewer: StackViewer) {
