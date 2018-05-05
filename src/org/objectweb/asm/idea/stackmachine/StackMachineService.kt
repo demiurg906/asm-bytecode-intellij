3f56a9b3bbd2932a5a97099980a1035ae1d03617
Roman Golyshev <anstreth@gmail.com>
2018-05-05 12:43:41 +0300

    add overloaded method for stack initialization

--- src/org/objectweb/asm/idea/stackmachine/StackMachineService.kt
+++ src/org/objectweb/asm/idea/stackmachine/StackMachineService.kt
@@ -8,12 +8,21 @@ import org.objectweb.asm.idea.ui.StackViewer
 
 typealias CommandsMap = Map<Int, Insn>
 
+/**
+ * Parameters to initialize stack state.
+ */
+data class StackParams(val commandsMap: CommandsMap, val localVariables: LocalVariableTable)
+
 interface StackMachineService {
     companion object {
         fun getInstance(project: Project): StackMachineService = ServiceManager.getService(project, StackMachineService::class.java)
     }
 
+    // TODO remove this
     fun initializeClass(map: CommandsMap)
+    fun initializeClass(params: StackParams) {
+        initializeClass(params.commandsMap)
+    }
     fun resetStack()
 
     fun emulateToCursor()
